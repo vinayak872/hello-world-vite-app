8a3621b9b9d2afeff786418d1d7189e14d0165a7
klu2300033530 <2300033530@kluniversity.in>
2025-07-31 11:46:52 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/hello-world-vite-app/',    REPO-NAME
   plugins: [react()],
-});
+  base: '/hello-world-vite-app/',
+})
